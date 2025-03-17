9ad6c5b8432f0c82afee52061a0d465ed6f28340
Manujayaa <janithrathnayake01@gmail.com>
2025-03-17 19:18:57 +0530

    create sign up page

--- waste_management/lib/main.dart
+++ waste_management/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:waste_management/screens/auth/sign_in_screen.dart';
+import 'package:waste_management/screens/auth/signup_screen.dart';
 import 'package:waste_management/screens/driver_screens/driver_home.dart';
 import 'package:waste_management/screens/resident_screens/resident_home.dart';
 import './utils/theme.dart';
@@ -21,11 +22,13 @@ class MyApp extends StatelessWidget {
       title: 'Waste Management System',
       theme: AppTheme.lightTheme,
       debugShowCheckedModeBanner: false,
-      initialRoute: '/sign_in_page', // Set initial route to HomeScreen
+      initialRoute: '/sign_up_page', // Set initial route to HomeScreen
       routes: {
         '/resident_home': (context) => const ResidentHome(), // Route for ResidentHomeScreen
         '/driver_home' : (context) => const DriverHome(), // Route for DriverHomeScreen
         '/sign_in_page' : (context) => const SignInPage(),
+        '/sign_up_page' : (context) => const SignupScreen(),
+
       },
     );
   }
